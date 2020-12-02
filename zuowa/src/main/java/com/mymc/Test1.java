317c2b3fe0a08096cb9e72201c197c2bac62e415
幕音暮城丶 <38254496+wtq1644@users.noreply.github.com>
2020-12-02 20:30:46 +0800

    关于成员变量及访问修饰符的测试

---
+++ zuowa/src/main/java/com/mymc/Test1.java
@@ -0,0 +1,36 @@
+package com.mymc;
+
+public class Test1 {
+
+    String yuju;
+    int a =7 ;
+
+    public String getYuju() {
+        return yuju;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setYuju(String yuju) {
+        this.yuju = yuju;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        test1.setYuju("676767");
+        test1.setA(give.b);
+        System.out.println(test1.getA());
+        System.out.println(test1.yuju);
+    }
+}
+
+class give{
+
+    static int b = 3;
+}
